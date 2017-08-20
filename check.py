1decef93621499319269e24d335935c3a4099d2e
光光 <kevinliu830829@163.com>
2017-08-20 18:11:57 +0800

    通过统计数据对异常流量进行预警， 通过已有统计数据对新加入流量进行预估

--- check.py
+++ check.py
@@ -3,13 +3,18 @@
 #VERSION:1.0
 #__OUTHOR__:guangguang
 class CalculationsEngine():
-    def __init__(self,FlowList,AmountOfUsers):
-        self.FlowList=FlowList
+    def __init__(self,FlowDictoonary,AmountOfUsers):
+        self.FlowList=FlowDictoonary
         self.AmountOfUsers=AmountOfUsers
+        self.UnitFlowList=[]
         pass
     def GetData(self):
         pass
-    def Calculations(self):
+    def CalculationsUnitFlow(self): #计算单位用户流量并进行排序
+        for Flow in self.FlowList:
+            self.UnitFlowList.append(Flow/self.AmountOfUsers)
+        self.UnitFlowList.sort()
+    def Calculation(self):
         pass
     def ReturnData(self):
         pass
