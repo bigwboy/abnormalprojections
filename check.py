455f94fd19c176d9e245f558642aae3f6bf39d64
光光 <kevinliu830829@163.com>
2017-08-20 18:00:08 +0800

    通过统计数据对异常流量进行预警， 通过已有统计数据对新加入流量进行预估

--- check.py
+++ check.py
@@ -2,3 +2,18 @@
 #time:2017/8/20 下午5:31
 #VERSION:1.0
 #__OUTHOR__:guangguang
+class CalculationsEngine():
+    def __init__(self,FlowList,AmountOfUsers):
+        self.FlowList=FlowList
+        self.AmountOfUsers=AmountOfUsers
+        pass
+    def GetData(self):
+        pass
+    def Calculations(self):
+        pass
+    def ReturnData(self):
+        pass
+
+
+if __name__=="__main__":
+    pass
